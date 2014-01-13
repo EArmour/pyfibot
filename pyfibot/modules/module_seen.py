3b9db2da41ea0aede7a58f4a1e5e9aab118168e1
Hargrimm <hargrimm@gmail.com>
2014-01-13 16:12:28 -0500

    Blank message for actions where it makes sense

--- pyfibot/modules/module_seen.py
+++ pyfibot/modules/module_seen.py
@@ -14,6 +14,7 @@ def init(bot):
         users = json.load(userfile)
         
 def command_seen(bot, user, channel, args):
+    """.seen [nick] - Returns the last time a user was active, and what they were doing"""
     global users
     
     user = args.lower()
@@ -26,7 +27,12 @@ def command_seen(bot, user, channel, args):
     diff = now-then
     diff = str(diff).split('.')[0] #remove microseconds
     
-    bot.say(channel, "%s last seen %s ago, %s with message '%s'" % (args, diff, users[user]['action'], users[user]['msg']))
+    if users[user]['msg']:
+        msg = "with message '%s'" % users[user]['msg']
+    else:
+        msg = "" 
+    
+    bot.say(channel, "%s was last seen %s ago, %s %s" % (args, diff, users[user]['action'], msg))
 
     with open(os.path.join(sys.path[0], 'modules', 'module_seen_users.json'),'w') as userfile:
         json.dump(users, userfile, indent=2)
@@ -34,8 +40,8 @@ def command_seen(bot, user, channel, args):
 def handle_privmsg(bot, user, channel, msg):
     update_user(user, "chatting", msg)
     
-# def handle_userJoined(bot, user, channel):
-#     update_user(user, "joining", channel)
+def handle_userJoined(bot, user, channel):
+    update_user(user, "joining")
     
 def handle_userLeft(bot, user, channel, message):
     update_user(user, "leaving", message)
@@ -44,12 +50,12 @@ def handle_userKicked(bot, kickee, channel, kicker, message):
     update_user(kickee + "!", "kicked by %s" % kicker, message)
     
 def handle_userRenamed(bot, oldnick, newnick):
-    update_user(oldnick, "changing name to %s" % newnick, "renamed")    
+    update_user(oldnick, "changing name to %s" % newnick)    
 
 def handle_action(bot, user, channel, data):
     update_user(user, "performing an action", data)
     
-def update_user(user, action, msg):
+def update_user(user, action, msg=None):
     global users
     users[getnick.get(user).lower()] = {'time': datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
                                         'action': action,
