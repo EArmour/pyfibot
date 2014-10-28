8902174f4c5a796d1bc68e2078e6b7dca1405bd0
Evan Armour <hargrimm@gmail.com>
2014-10-28 00:52:31 -0400

    Update metacritic call

--- pyfibot/modules/module_metacritic.py
+++ pyfibot/modules/module_metacritic.py
@@ -6,12 +6,20 @@ from bs4 import BeautifulSoup
 
 def command_score(bot, user, channel, args):
     game = args.replace(' ', '+')
-    page = urllib2.urlopen("http://www.metacritic.com/search/all/%s/results" % game).read()
+
+    req = urllib2.Request("http://www.metacritic.com/search/all/%s/results" % game)
+    req.add_header('User-Agent', 'Mozilla/5.0 (Windows NT 6.3; WOW64; rv:33.0) Gecko/20100101 Firefox/33.0')
+    req.add_header('Host', 'www.metacritic.com')
+
+    page = urllib2.urlopen(req).read()
     bs = BeautifulSoup(page)
     
     scoretag = bs.find(class_="metascore_w")
-    score = scoretag.text
-    titletag = scoretag.previous_sibling.previous_sibling
-    title = titletag.contents[0].text
-    
-    bot.say(channel, "MetaCritic: %s -- Avg Score %s" % (title, score))
+    try:
+        score = scoretag.text
+        titletag = bs.find(class_="product_title")
+        title = titletag.text
+
+        bot.say(channel, "Metacritic: %s -- Avg Score %s" % (title, score))
+    except:
+        bot.say(channel, "Couldn't find any Metacritic score for '%s'" % args)
